053bd983d0f4f406b2cd51ed7a834b7292c96217
Gago <egagik69@gmail.com>
2025-04-01 21:27:09 +0400

    create fonts and routh

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { MeProfileComponent } from './pages/me-profile/me-profile.component';
+import { ChatComponent } from './pages/chat/chat.component';
+import { SearchComponent } from './pages/search/search.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component:LoginComponent
+    },
+    {
+        path:'me-profile',
+        component:MeProfileComponent
+    },
+    {
+        path:'chat',
+        component:ChatComponent
+    },
+    {
+        path:'search',
+        component:SearchComponent
+    },
+    {
+        path:'**',
+        component:NotFoundComponent
+    },
+];
---
+++ src/app/pages/me-profile/me-profile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-me-profile',
+  imports: [],
+  templateUrl: './me-profile.component.html',
+  styleUrl: './me-profile.component.scss'
+})
+export class MeProfileComponent {
+
+}
