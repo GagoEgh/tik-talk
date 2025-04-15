e8393455354714f1b8e7b32a2ee8047785fc5d76
Gago <egagik69@gmail.com>
2025-04-15 23:25:23 +0400

    create login form

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormGroup, NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { LoginFormInterface } from '../../core/interface/login.interface';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  private fb = inject(NonNullableFormBuilder);
 
+  public eyeOn = signal<boolean>(false);
+  public loginForm:FormGroup<LoginFormInterface> =this.fb.group({
+    login:['',Validators.required],
+    password:['',Validators.required]
+  })
+ 
+  public changeVisibility():void{
+    this.eyeOn.update((value)=>value = !value)
+  }
+  
+  public send():void{
+    console.log(this.loginForm);
+  }
 }
